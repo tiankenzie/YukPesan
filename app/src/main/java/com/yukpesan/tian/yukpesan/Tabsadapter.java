82b31a928e44061e888ff96376afea5f5cec4448
Gasik Sambada <gasik@bigdutyfree.com>
2016-06-11 11:44:38 +0700

    tabsadapter

---
+++ app/src/main/java/com/yukpesan/tian/yukpesan/Tabsadapter.java
@@ -0,0 +1,34 @@
+package com.yukpesan.tian.yukpesan;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Gasik on 6/8/2016.
+ */
+public class Tabsadapter extends FragmentStatePagerAdapter {
+    private int TOTAL_TABS = 2;
+
+    public Tabsadapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FragHome();
+
+            case 1:
+                return new FragProfile();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return TOTAL_TABS;
+    }
+}

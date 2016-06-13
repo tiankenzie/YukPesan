74e7f5b277b84750d75296f6dacccf078e4f9442
prabowo agustian <sumatona.otoko@gmail.com>
2016-06-13 22:05:50 +0700

    four commit

--- app/src/main/java/com/yukpesan/tian/yukpesan/MainActivity.java
+++ app/src/main/java/com/yukpesan/tian/yukpesan/MainActivity.java
@@ -48,6 +48,72 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        TextView meja3 = (TextView) this.findViewById(R.id.meja3);
+        meja3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedpreferences = getSharedPreferences(MainActivity.MyPreference, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("meja", "3");
+                Intent intent =new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
+        });
+        TextView meja4 = (TextView) this.findViewById(R.id.meja4);
+        meja4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedpreferences = getSharedPreferences(MainActivity.MyPreference, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("meja", "4");
+                Intent intent =new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
+        });
+        TextView meja5 = (TextView) this.findViewById(R.id.meja5);
+        meja5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedpreferences = getSharedPreferences(MainActivity.MyPreference, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("meja", "5");
+                Intent intent =new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
+        });
+        TextView meja6 = (TextView) this.findViewById(R.id.meja6);
+        meja6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedpreferences = getSharedPreferences(MainActivity.MyPreference, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("meja", "6");
+                Intent intent =new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
+        });
+        TextView meja7 = (TextView) this.findViewById(R.id.meja7);
+        meja7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedpreferences = getSharedPreferences(MainActivity.MyPreference, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("meja", "7");
+                Intent intent =new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
+        });
+        TextView meja8 = (TextView) this.findViewById(R.id.meja8);
+        meja8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedpreferences = getSharedPreferences(MainActivity.MyPreference, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("meja", "8");
+                Intent intent =new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
+        });
     }
 
 

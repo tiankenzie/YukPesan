2ca12b5a2ca7ca678557946d0ff5d9d3f12317a0
prabowo agustian <sumatona.otoko@gmail.com>
2016-06-25 08:03:42 +0700

    spinner commit

--- app/src/main/java/com/yukpesan/tian/yukpesan/detail_menu.java
+++ app/src/main/java/com/yukpesan/tian/yukpesan/detail_menu.java
@@ -3,13 +3,20 @@ package com.yukpesan.tian.yukpesan;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 /**
  * Created by User on 6/20/2016.
  */
 public class detail_menu extends AppCompatActivity {
-TextView title;
+    TextView title;
+    Spinner detail_menu;
+
+    String[] quantity = {"1", "2","3", "4", "5", "6", "7", "8", "9", "10"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +25,14 @@ TextView title;
         getSupportActionBar().setCustomView(R.layout.actionbar1);
         setContentView(R.layout.detail_menu);
 
+
         title = (TextView) findViewById(R.id.title);
         title.setText("Detail Menu");
+
+        detail_menu = (Spinner)findViewById(R.id.spinner_menu);
+
+        ArrayAdapter<String>detail_menuadpter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, quantity);
+        detail_menu.setAdapter(detail_menuadpter);
+
     }
 }

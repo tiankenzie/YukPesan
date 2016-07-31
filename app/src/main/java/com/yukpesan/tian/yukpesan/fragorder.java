1a407789d919b3bce0181b4c7dff692d1187ab35
prabowo agustian <sumatona.otoko@gmail.com>
2016-07-31 17:32:11 +0700

    commit euy

--- app/src/main/java/com/yukpesan/tian/yukpesan/fragorder.java
+++ app/src/main/java/com/yukpesan/tian/yukpesan/fragorder.java
@@ -1,19 +1,43 @@
 package com.yukpesan.tian.yukpesan;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by User on 6/11/2016.
  */
 public class fragorder extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragorder, container, false);
+        //return inflater.inflate(R.layout.fragorder, container, false);
 
 
+        View fview = inflater.inflate(R.layout.fragorder,container, false);
+        Button button1 = (Button) fview.findViewById(R.id.order);
+        Button button2 = (Button) fview.findViewById(R.id.pay);
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getActivity(), page_koki.class);
+                startActivity(i);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getActivity(), page_kasir.class);
+                startActivity(i);
+            }
+        });
+        return fview;
     }
 }
